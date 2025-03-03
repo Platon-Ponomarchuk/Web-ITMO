6dcaee5cc723e65174c62c157bf9e834e967a97b
Platon Ponomarchuk <platonponomarchuk@yandex.ru>
2025-03-03 09:44:45 +0300

    feat: add test card for error fetch

--- scripts/API.js
+++ scripts/API.js
@@ -32,21 +32,41 @@ export function getData() {
 				price.textContent = "100 ₽";
 				button.textContent = "Добавить";
 
-				button.addEventListener("click", () => {
-					cart.push(item);
-					console.log(cart);
-				});
-
 				catalog_item.append(image, title, price, button);
 				catalog.append(catalog_item);
 			});
 		})
-		.catch((error) => {
+		.catch(() => {
 			document.querySelector(".loader").style.display = "none";
 			document.querySelector(".main__catalog").style.display = "flex";
 			document.querySelector("#more").style.display = "none";
 			const title = document.createElement("h2");
 			title.textContent = "Произошла ошибка";
+			document.querySelector(".main__catalog").style.display = "flex";
+			const catalog_item = document.createElement("li");
+			const image = document.createElement("img");
+			const titleCard = document.createElement("h3");
+			const price = document.createElement("p");
+			const button = document.createElement("button");
+
+			catalog_item.classList.add("main__catalog_item");
+			image.classList.add("item__image");
+			title.classList.add("item__title");
+			price.classList.add("item__price");
+			button.classList.add("item__button");
+			// Атрибуты для работы с модальным окном Bootstrap
+			// data-bs-toggle - атрибут для открытия модального окна
+			// data-bs-target - атрибут для указания идентификатора модального окна
+			button.setAttribute("data-bs-toggle", "modal");
+			button.setAttribute("data-bs-target", "#modal");
+
+			image.src = "./images/test.jpg";
+			titleCard.textContent = "Test";
+			price.textContent = "100 ₽";
+			button.textContent = "Добавить";
+
+			catalog_item.append(image, titleCard, price, button);
+			catalog.append(catalog_item);
 			catalog.append(title);
 		});
 }
