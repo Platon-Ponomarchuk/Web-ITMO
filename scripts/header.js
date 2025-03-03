3d45dfe9874306d6162dd67741c4126a5ab3e0a7
Platon Ponomarchuk <platonponomarchuk@yandex.ru>
2025-03-03 09:57:24 +0300

    fix: fix pathnames conditions

--- scripts/header.js
+++ scripts/header.js
@@ -2,11 +2,11 @@ const main_page_button = document.querySelector("#home");
 const profile_page_button = document.querySelector("#profile");
 const toBuy_page_button = document.querySelector("#toBuy");
 
-if(document.location.pathname === "/profile.html") {
+if(document.location.pathname.includes("profile.html")) {
 	main_page_button.classList.remove("header__menu_item_active");
 	toBuy_page_button.classList.remove("header__menu_item_active");
 	profile_page_button.classList.add("header__menu_item_active");
-} else if (document.location.pathname === "/toBuy.html") {
+} else if (document.location.pathname.includes("toBuy.html")) {
 	main_page_button.classList.remove("header__menu_item_active");
 	profile_page_button.classList.remove("header__menu_item_active");
 	toBuy_page_button.classList.add("header__menu_item_active");
